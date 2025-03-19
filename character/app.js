c1f109d240615b55e75d1a47c655d7e27012249a
elias hamidi <hamidielias59@gmail.com>
2025-03-19 16:33:30 +0100

    ...

--- character/app.js
+++ character/app.js
@@ -1,8 +1,8 @@
 const { Guerrier, Mage, Voleur } = require('./classes');
 
-const guerrier = new Guerrier("Aragorn");
-const mage = new Mage("Gandalf");
-const voleur = new Voleur("Robin");
+const guerrier = new Guerrier("staline");
+const mage = new Mage("hitler");
+const voleur = new Voleur("maximu");
 
 guerrier.displayCharacter();
 mage.displayCharacter();
